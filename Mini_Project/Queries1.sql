0939d9f44787e6113c27f5d95aef7a3ddf0d79c8
ssgandham <gandhamsnehalsrinivas@gmail.com>
2017-10-23 09:26:40 -0700

    New code addded for Query 5

--- Mini_Project/Queries1.sql
+++ Mini_Project/Queries1.sql
@@ -16,7 +16,7 @@ group by (a.followee)
 order by b.name;
 
 -- 3
-select a.name,count(b.blurtid) as blurt_count from user a
+select a.name,count(b.blurtid) as blurt_count from user 
 inner join blurt b
 on a.email=b.email
 where a.type='C'
@@ -25,3 +25,16 @@ order by blurt_count desc;
 
 -- 4
 select name from user where email not in (select follower from follow) and type='C';
+
+-- 5
+select a.name, count(*) from vendor_ambassador a 
+inner join vendor b on a.vendorid=b.id
+inner join follow c on a.email=c.followee
+group by (b.email);
+
+-- 6 
+select * from blurt_analysis a
+inner join vendor_topics b on a.topicid=b.topicid
+inner join vendor c on c.id=b.vendorid
+group by c.name
+order by a.confidence;
