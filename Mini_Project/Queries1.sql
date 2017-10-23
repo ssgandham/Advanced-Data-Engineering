76169b40b205a2b5087625b016a88202cbc8bea5
ssgandham <gandhamsnehalsrinivas@gmail.com>
2017-10-22 17:09:17 -0700

    @nd Query is working

--- Mini_Project/Queries1.sql
+++ Mini_Project/Queries1.sql
@@ -7,3 +7,10 @@ inner join topic b on a.topicid=b.id
 group by topicid
 order by topicid;
 
+
+-- 2
+select b.name, count(a.follower) 
+from follow a inner join user b
+on a.followee = b.email
+group by (a.followee)
+order by b.name;
